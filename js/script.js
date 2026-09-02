de520f4d0703e6b5ec7694b45379626361625228
André Zammit <mawzer@gmail.com>
2026-09-03 01:17:48 +0200

    Refreshed the PostsDash website.

--- js/script.js
+++ js/script.js
@@ -1,98 +1,65 @@
 const Browsers = {
-	Unknown: 0,
-	Chrome: 1,
-	Edge: 2,
-	Firefox: 3,
-    Safari: 4,
-    Opera: 5
-}
+    Unknown: 0,
+    Chrome: 1,
+    Edge: 2,
+    Firefox: 3,
+    Opera: 4
+};
 
 function detectBrowser() {
-    var userAgent = navigator.userAgent;
+    const userAgent = navigator.userAgent;
 
-    if (userAgent.indexOf("Edg/") > -1) {
+    if (userAgent.includes("Edg/")) {
         return Browsers.Edge;
-    } else if (userAgent.indexOf("OPR/") > -1) {
+    }
+
+    if (userAgent.includes("OPR/")) {
         return Browsers.Opera;
-    } else if (userAgent.indexOf("Chrome/") > -1) {
+    }
+
+    if (userAgent.includes("Chrome/")) {
         return Browsers.Chrome;
-    } else if (userAgent.indexOf("Firefox/") > -1) {
+    }
+
+    if (userAgent.includes("Firefox/")) {
         return Browsers.Firefox;
-    } else {
-        return Browsers.Unknown;
     }
-}
 
-function getKeyByValue(object, value) {
-    return Object.keys(object).find(key => object[key] === value);
-}
-
-function getBrowserLink(detectedBrowser) {
-    switch (detectedBrowser) {
-        case Browsers.Chrome:
-            return "chrome";
-
-        case Browsers.Edge:
-            return "edge";
-
-        case Browsers.Firefox:
-            return "firefox";
-
-        case Browsers.Opera:
-            return "opera";
-
-        default:
-            return "#browsers";
-    }
-}
-
-function getInstallButtonText(detectedBrowser) {
-    switch (detectedBrowser) {
-        case Browsers.Chrome:
-            return "Install Chrome extension";
-
-        case Browsers.Edge:
-            return "Install Edge extension";
-
-        case Browsers.Firefox:
-            return "Install Firefox extension";
-
-        case Browsers.Opera:
-            return "Install Opera extension";
-
-        default:
-            return "Unsupported browser";
-    }
+    return Browsers.Unknown;
 }
 
 function getInstallButtonInfo(detectedBrowser) {
-    const buttonLink = getBrowserLink(detectedBrowser);
-    const buttonText = getInstallButtonText(detectedBrowser);
-
-    return { 
-        "link": buttonLink, 
-        "text": buttonText,
-        "browser": detectedBrowser
-    };
+    switch (detectedBrowser) {
+        case Browsers.Chrome:
+            return { link: "chrome", text: "Install for Chrome", browser: detectedBrowser };
+        case Browsers.Edge:
+            return { link: "edge", text: "Install for Edge", browser: detectedBrowser };
+        case Browsers.Firefox:
+            return { link: "firefox", text: "Install for Firefox", browser: detectedBrowser };
+        case Browsers.Opera:
+            return { link: "opera", text: "Install for Opera", browser: detectedBrowser };
+        default:
+            return { link: "#browsers", text: "Choose your browser", browser: detectedBrowser };
+    }
 }
 
-function initInstallButton(installButtonInfo) {
+function initInstallButton() {
     const installButtonLink = document.querySelector(".installButtonLink");
-    installButtonLink.setAttribute("href", installButtonInfo.link);
+    const installButton = document.querySelector(".installButton");
 
-    if (installButtonInfo.browser === Browsers.Unknown) {
-        installButtonLink.removeAttribute("target");
+    if (installButtonLink === null || installButton === null) {
+        return;
     }
 
-    const installButton = document.querySelector(".installButton");
+    const installButtonInfo = getInstallButtonInfo(detectBrowser());
+
+    installButtonLink.setAttribute("href", installButtonInfo.link);
     installButton.textContent = installButtonInfo.text;
+
+    if (installButtonInfo.browser !== Browsers.Unknown) {
+        installButtonLink.setAttribute("target", "_blank");
+        installButtonLink.setAttribute("rel", "noopener noreferrer");
+    }
 }
 
-window.onload = function() {
-    const detectedBrowser = detectBrowser();
-
-    console.log("PostsDash: Browser detected: " + getKeyByValue(Browsers, detectedBrowser));
-
-    const installButtonInfo = getInstallButtonInfo(detectedBrowser);
-    initInstallButton(installButtonInfo);
-}
+document.addEventListener("DOMContentLoaded", initInstallButton);
